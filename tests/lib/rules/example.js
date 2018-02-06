2cac261da110954f0c0534a578dbd6360ab229a2
Avinash Varma <s.avinash.v@uber.com>
2018-02-06 15:11:42 +0530

    add getModuleMeta method to get absolute & relative paths of a import declaration

--- tests/lib/rules/example.js
+++ tests/lib/rules/example.js
@@ -1,19 +1,45 @@
-var rule = require('../../../lib/rules/example')
+var path = require('path');
+var rule = require('../../../lib/rules/example');
+var test = require('/Users/avinashvarma/Downloads/Downloads/oss/eslint-plugin-import-comments/tests/lib/files/index.js');
 var RuleTester = require('eslint').RuleTester;
+RuleTester.setDefaultConfig({
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module"
+  }
+});
 
 var ruleTester = new RuleTester();
 
 ruleTester.run('example', rule, {
   valid: [
-    'var validExample;',
-    'for(;;) {}',
-    'do {} while(condition)'
-  ],
-
-  invalid: [
     {
-      code: 'while(condition) {}',
-      errors: [ { message: 'Do not use while loops.' } ]
+      code: `
+        // test comment
+        import x from "./existent-file";
+        import y from "./existent-file-2";
+      `,
+      filename: path.join(process.cwd(), './tests/lib/files/')
+    },
+    {
+      code: 'import x from "path";',
+      filename: path.join(process.cwd(), './tests/lib/files/')
     }
-  ]
+  ],
+  invalid: [{
+    code: `
+      import x from "./non-existent-file";
+    `,
+    filename: path.join(process.cwd(), './tests/lib/files/'),
+    errors: [{
+      message: 'Couldnt find the module'
+    }]
+  },
+  {
+    code: 'import x from "no-pkg";',
+    filename: path.join(process.cwd(), './tests/lib/files/'),
+    errors: [{
+      message: 'Couldnt find the module',
+    }]
+  }]
 });
